2b214275b34a677ea86ca62597de8bf698e46de1
musashi <83016329+musash1@users.noreply.github.com>
2022-12-15 21:37:50 +0100

    Delete day1/target directory

--- day1/target/debug/deps/day1-fad5a59e3cba762f.d
+++
@@ -1,5 +0,0 @@
-c:\Users\luisg\Documents\GitHub\advent-of-code\day1\target\debug\deps\day1-fad5a59e3cba762f.rmeta: src\main.rs
-
-c:\Users\luisg\Documents\GitHub\advent-of-code\day1\target\debug\deps\day1-fad5a59e3cba762f.d: src\main.rs
-
-src\main.rs:
--- day1/target/debug/deps/day1.d
+++
@@ -1,5 +0,0 @@
-C:\Users\luisg\Documents\GitHub\advent-of-code\day1\target\debug\deps\day1.exe: src\main.rs
-
-C:\Users\luisg\Documents\GitHub\advent-of-code\day1\target\debug\deps\day1.d: src\main.rs
-
-src\main.rs:
