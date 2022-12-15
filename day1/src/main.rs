80991c1b8639c6e395a699a0931b6dc1fc3c21a2
luis <83016329+musash1@users.noreply.github.com>
2022-12-15 22:03:12 +0100

    day1 part2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -8,7 +8,7 @@ fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
 
-fn find_biggest_number(lines: Vec<String>) -> i32 {
+fn create_vector_with_calories(lines: Vec<String>) -> Vec<i32> {
     let mut vec = Vec::new();
     let mut temp: i32 = 0;
     for line in lines {
@@ -20,12 +20,28 @@ fn find_biggest_number(lines: Vec<String>) -> i32 {
         temp += line.parse::<i32>().unwrap();
     } 
 
-    *vec.iter().max().unwrap()
+    vec
+}
+
+fn search_sum_of_three_biggest_elves(calories: &mut Vec<i32>) -> i32 {
+    let max: i32 = *calories.iter().max().unwrap();
+    let mut sum: i32 = 0;
+    let mut i = 0;
+    while i < 3 {
+        sum += *calories.iter().max().unwrap();
+        let index: usize = calories.iter().position(|x| *x == *calories.iter().max().unwrap()).unwrap();
+        calories.remove(index);
+        i += 1;
+    } 
+
+    sum
 }
 
 fn main() {
     let lines = lines_from_file("src/day1.txt").expect("Could not load lines");
-    let max: i32 = find_biggest_number(lines);
+    let mut vec: Vec<i32> = create_vector_with_calories(lines);
+    let sum: i32 = search_sum_of_three_biggest_elves(&mut vec);
 
-    println!("{}", max);
+
+    println!("{}", sum);
 }
