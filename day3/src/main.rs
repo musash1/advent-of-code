09d4105bb034aa222eb6cfa87566e9f58340f600
musash1 <luis.gjokaj@gmail.com>
2023-02-27 23:08:35 +0100

    programm giving number back, still wrong value

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -4,18 +4,38 @@ use std::{
     path::Path,
 };
 
+static ASCII_LOWER: [char; 26] = [
+    'a', 'b', 'c', 'd', 'e', 
+    'f', 'g', 'h', 'i', 'j', 
+    'k', 'l', 'm', 'n', 'o',
+    'p', 'q', 'r', 's', 't', 
+    'u', 'v', 'w', 'x', 'y', 
+    'z',
+];
+
+static ASCII_HIGHER: [char; 26] = [
+    'A', 'B', 'C', 'D', 'E', 
+    'F', 'G', 'H', 'I', 'J', 
+    'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T', 
+    'U', 'V', 'W', 'X', 'Y', 
+    'Z',
+];
+
 fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
     BufReader::new(File::open(filename)?).lines().collect()
 }
 
-fn check_for_same_letter(lines: &Vec<String>) -> Vec<&String> {
-    let mut return_value: Vec<&String> = vec![];
+fn check_for_same_letter(lines: &Vec<String>) -> Vec<char> {
+    let mut return_value: Vec<char> = vec![];
     for line in lines {
-        let (arr1, arr2) = lines.split_at(line.len() / 2);
+       let (str1, str2) = line.split_at(line.len() / 2);
+        let arr1: Vec<char> = str1.chars().collect();
+        let arr2: Vec<char> = str2.chars().collect();
         for i in 0..arr1.len() {
             for j in 0..arr2.len() {
                 if arr1[i] == arr2[j] {
-                    return_value.push(&arr1[i]);
+                    return_value.push(arr1[i]);
                 }
             }
         }
@@ -24,20 +44,26 @@ fn check_for_same_letter(lines: &Vec<String>) -> Vec<&String> {
     return_value
 } 
 
-fn get_value(letters: &Vec<&String>) -> i32 {
+fn get_value(letters: &Vec<char>) -> usize {
     let mut value = 0;
     for letter in letters {
-        value = value + letter.parse::<i32>().unwrap() - 64; 
+        for i in 0..25 {
+            if *letter == ASCII_LOWER[i] {
+                value += i;
+            }
+            if *letter == ASCII_HIGHER[i] {
+                value += i + 26;
+            }
+        }
     }
     value
 }
 
 fn main() {
     let lines: Vec<String> = lines_from_file("src/day3.txt").expect("Could not load lines");
-    let letters: Vec<&String> = check_for_same_letter(&lines);
-    let value: i32 = get_value(&letters);
+    let letters: Vec<char> = check_for_same_letter(&lines);
+    let value: usize = get_value(&letters);
 
     println!("{}", value);
 }
-
 
