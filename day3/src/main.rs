d9030d3b1e694b5843f93986b60ad3ad4b36d566
ruuiisu <luis.gjokaj@gmail.com>
2023-02-28 16:14:23 +0100

    completed part1 changed it with best practices and finished part 2

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -1,69 +1,54 @@
 use std::{
-    fs::File,
-    io::{self, BufRead, BufReader},
     path::Path,
 };
 
-static ASCII_LOWER: [char; 26] = [
-    'a', 'b', 'c', 'd', 'e', 
-    'f', 'g', 'h', 'i', 'j', 
-    'k', 'l', 'm', 'n', 'o',
-    'p', 'q', 'r', 's', 't', 
-    'u', 'v', 'w', 'x', 'y', 
-    'z',
-];
+use itertools::Itertools;
 
-static ASCII_HIGHER: [char; 26] = [
-    'A', 'B', 'C', 'D', 'E', 
-    'F', 'G', 'H', 'I', 'J', 
-    'K', 'L', 'M', 'N', 'O',
-    'P', 'Q', 'R', 'S', 'T', 
-    'U', 'V', 'W', 'X', 'Y', 
-    'Z',
-];
-
-fn lines_from_file(filename: impl AsRef<Path>) -> io::Result<Vec<String>> {
-    BufReader::new(File::open(filename)?).lines().collect()
+fn lines_from_file(filename: impl AsRef<Path>) -> String {
+    std::fs::read_to_string(filename).unwrap()
 }
 
-fn check_for_same_letter(lines: &Vec<String>) -> Vec<char> {
-    let mut return_value: Vec<char> = vec![];
-    for line in lines {
-       let (str1, str2) = line.split_at(line.len() / 2);
-        let arr1: Vec<char> = str1.chars().collect();
-        let arr2: Vec<char> = str2.chars().collect();
-        for i in 0..arr1.len() {
-            for j in 0..arr2.len() {
-                if arr1[i] == arr2[j] {
-                    return_value.push(arr1[i]);
-                }
-            }
-        }
-    }
+fn part1(lines: &String) -> u32 {
+    lines
+        .lines()
+        // calculate the priority of the single unique item for each line
+        .filter_map(|line| {
+            let line = line.as_bytes();
+            let (left, right) = line.split_at(line.len() / 2);
 
-    return_value
+            left.iter()
+                .find(|item| right.contains(item))
+                .map(|item| match item {
+                    b'a'..=b'z' => (item - b'a') + 1,
+                    _ => (item - b'A') + 1 + 26,
+                } as u32)
+        })
+        .sum()
+}
+
+pub fn part2(lines: &String) -> u32 {
+    lines
+        .lines()
+        .map(|line| line.as_bytes())
+        .tuples()
+        .filter_map(|(sack1, sack2, sack3)| {
+           sack1
+            .iter()
+            .find(|item| sack2.contains(item) && sack3.contains(item))
+            .map(|item| match item {
+                    b'a'..=b'z' => (item - b'a') + 1,
+                    _ => (item - b'A') + 1 + 26,
+                } as u32)
+        })
+            .sum()
 } 
 
-fn get_value(letters: &Vec<char>) -> usize {
-    let mut value = 0;
-    for letter in letters {
-        for i in 0..25 {
-            if *letter == ASCII_LOWER[i] {
-                value += i;
-            }
-            if *letter == ASCII_HIGHER[i] {
-                value += i + 26;
-            }
-        }
-    }
-    value
-}
-
 fn main() {
-    let lines: Vec<String> = lines_from_file("src/day3.txt").expect("Could not load lines");
-    let letters: Vec<char> = check_for_same_letter(&lines);
-    let value: usize = get_value(&letters);
+    let lines: String = lines_from_file("src/day3.txt");
+    let value: u32 = part1(&lines);
+    let value2: u32 = part2(&lines);
 
     println!("{}", value);
+    println!("{}", value2);
 }
 
