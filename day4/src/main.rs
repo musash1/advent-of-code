ebd83948b9d130bf96ff99e2de54de5dcfc39a53
musash1 <luis.gjokaj@gmail.com>
2023-03-06 13:39:24 +0100

    fixed Option error

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,16 +1,14 @@
-use std::{
-    path::Path,
-};
+use std::path::Path;
 
 fn lines_from_file(filename: impl AsRef<Path>) -> String {
     std::fs::read_to_string(filename).unwrap()
 }
 
 fn part_1(lines: &String) -> i32 {
-    let value: i32 = 0;
+    let mut value: i32 = 0;
     lines
         .lines()
-        .filter_map(|line| {
+        .map(|line| {
             let splittet_line: Vec<&str> = line.split(',').collect();
             let left_splittet_line: Vec<&str> = splittet_line[0].split('-').collect();
             let right_splittet_line: Vec<&str> = splittet_line[1].split('-').collect();
@@ -20,13 +18,16 @@ fn part_1(lines: &String) -> i32 {
             if left_vec.iter().all(|item| right_vec.contains(item)) || right_vec.iter().all(|item| left_vec.contains(item)) {
                 value += 1;
             }
-        }); 
+        });
 
     value
 }
 
 fn main() {
-    let lines: String = lines_from_file("src/day3.txt");
+    let lines: String = lines_from_file("src/day4.txt");
     let value: i32 = part_1(&lines);
+
+    println!("{}", lines);
+    println!("{}", value);
 }
 
