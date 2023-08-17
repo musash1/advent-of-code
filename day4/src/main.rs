14d527e24aebd9737222b9c1fa21bf33d08078f3
Gjokaj Luis (GJL) <luis.gjokaj@suva.ch>
2023-08-17 13:14:38 +0200

    day4 part 1 finished

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -1,33 +1,27 @@
-use std::path::Path;
-
-fn lines_from_file(filename: impl AsRef<Path>) -> String {
-    std::fs::read_to_string(filename).unwrap()
-}
+use std::fs::read_to_string;
 
 fn part_1(lines: &String) -> i32 {
     let mut value: i32 = 0;
-    lines
-        .lines()
-        .map(|line| {
+    for line in lines.lines() {
             let splittet_line: Vec<&str> = line.split(',').collect();
             let left_splittet_line: Vec<&str> = splittet_line[0].split('-').collect();
             let right_splittet_line: Vec<&str> = splittet_line[1].split('-').collect();
-            let left_vec = vec![left_splittet_line[0].parse::<i32>().unwrap()..left_splittet_line[1].parse::<i32>().unwrap()];
-            let right_vec = vec![right_splittet_line[0].parse::<i32>().unwrap()..right_splittet_line[1].parse::<i32>().unwrap()];
-
-            if left_vec.iter().all(|item| right_vec.contains(item)) || right_vec.iter().all(|item| left_vec.contains(item)) {
+            let left_range = left_splittet_line[0].parse::<i32>().unwrap()..=left_splittet_line[1].parse::<i32>().unwrap();
+            let right_range = right_splittet_line[0].parse::<i32>().unwrap()..=right_splittet_line[1].parse::<i32>().unwrap();
+            
+            if (left_range.contains(&right_range.start()) && left_range.contains(&right_range.end())) ||
+               (right_range.contains(&left_range.start()) && right_range.contains(&left_range.end())) {
                 value += 1;
             }
-        });
-
+        }
     value
 }
 
 fn main() {
-    let lines: String = lines_from_file("src/day4.txt");
+    let lines: String = read_to_string("src/day4.txt").unwrap();
     let value: i32 = part_1(&lines);
 
-    println!("{}", lines);
+
     println!("{}", value);
 }
 
