aca0240718d32d452b9e8ec9da33df3bed66d10b
Gjokaj Luis (GJL) <luis.gjokaj@suva.ch>
2023-08-17 13:19:19 +0200

    day4 part 2 finished

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -17,11 +17,29 @@ fn part_1(lines: &String) -> i32 {
     value
 }
 
+fn part_2(lines: &String) -> i32 {
+    let mut value: i32 = 0;
+    for line in lines.lines() {
+            let splittet_line: Vec<&str> = line.split(',').collect();
+            let left_splittet_line: Vec<&str> = splittet_line[0].split('-').collect();
+            let right_splittet_line: Vec<&str> = splittet_line[1].split('-').collect();
+            let left_range = left_splittet_line[0].parse::<i32>().unwrap()..=left_splittet_line[1].parse::<i32>().unwrap();
+            let right_range = right_splittet_line[0].parse::<i32>().unwrap()..=right_splittet_line[1].parse::<i32>().unwrap();
+            
+            if (left_range.contains(&right_range.start()) || left_range.contains(&right_range.end())) ||
+               (right_range.contains(&left_range.start()) || right_range.contains(&left_range.end())) {
+                value += 1;
+            }
+        }
+    value
+}
+
 fn main() {
     let lines: String = read_to_string("src/day4.txt").unwrap();
-    let value: i32 = part_1(&lines);
+    let part1: i32 = part_1(&lines);
+    let part2: i32 = part_2(&lines);
 
-
-    println!("{}", value);
+    println!("{}", part1);
+    println!("{}", part2)
 }
 
