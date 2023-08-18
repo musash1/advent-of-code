1d87e8cec40ffb0f7e230528635d9a1842911458
musash1 <luis.gjokaj@gmail.com>
2023-08-18 17:10:12 +0200

    started day5

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,12 +1,23 @@
 use std::fs::read_to_string;
+use regex::Regex;
 
-fn part_1(lines: &String) -> i32 {
-    0
+fn parse_crates(lines: &String) -> &str {
+    let mut first_vec: Vec<&str> = Vec::new();
+    for line in lines.lines() {
+         
+    }
+    "hello"
+}
+
+fn part_1(lines: &String) -> &str {
+    let crates: &str = parse_crates(lines);
+    "Hello"
 }
 
 fn main() {
     let lines: String = read_to_string("src/day5.txt").unwrap();
-    let part1: i32 = part_1(&lines);
+    let part1: &str = part_1(&lines);
 
     println!("{}", part1);
 }
+
