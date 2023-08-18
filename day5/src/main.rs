13880b54a731485bd567d1f46160f1fd35713446
Gjokaj Luis (GJL) <luis.gjokaj@suva.ch>
2023-08-18 13:35:46 +0200

    added day5

---
+++ day5/src/main.rs
@@ -0,0 +1,12 @@
+use std::fs::read_to_string;
+
+fn part_1(lines: &String) -> i32 {
+    0
+}
+
+fn main() {
+    let lines: String = read_to_string("src/day5.txt").unwrap();
+    let part1: i32 = part_1(&lines);
+
+    println!("{}", part1);
+}
