8748ec66ac50b647898711c2155ca23e67ce497f
Gjokaj Luis (GJL) <luis.gjokaj@suva.ch>
2025-11-26 09:26:07 +0100

    day6 complete

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,3 +1,38 @@
+use std::fs::read_to_string;
+
+fn part_1(lines: &str) -> usize {
+    let mut temp = String::from("");
+    for (i, c) in lines.chars().enumerate() {
+        if let Some(pos) = temp.find(c) {
+            temp.drain(..=pos);
+        }
+        temp.push(c);
+        if temp.len() == 4 {
+            return i + 1;
+        }
+    }
+    0
+}
+
+fn part_2(lines: &str) -> usize {
+    let mut temp = String::from("");
+    for (i, c) in lines.chars().enumerate() {
+        if let Some(pos) = temp.find(c) {
+            temp.drain(..=pos);
+        }
+        temp.push(c);
+        if temp.len() == 14 {
+            return i + 1;
+        }
+    }
+    0
+}
+
 fn main() {
-    println!("Hello, world!");
+    let lines: String = read_to_string("src/test.txt").unwrap();
+    let part1 = part_1(&lines);
+    let part2 = part_2(&lines);
+
+    println!("part1: {}", part1);
+    println!("part2: {}", part2);
 }
